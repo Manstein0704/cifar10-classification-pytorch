Fix CIFAR-10 normalization

dataset.py
@@ -1,25 +1,25 @@
-import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 
-
+CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
+CIFAR10_STD = (0.2470, 0.2435, 0.2616)
 
 def get_transforms():
-    trin_transform = transforms.Compose([
+    train_transform = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
-        transforms.Normalize(0.5, 0.5, 0.5),
+        transforms.Normalize(mean=CIFAR10_MEAN, std=CIFAR10_STD),
         transforms.RandomErasing(p=0.3, scale=(0.02, 0.33), ratio=(0.3, 3.3), inplace=False)
     ])
 
     test_transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(0.5, 0.5, 0.5)
+        transforms.Normalize(mean=CIFAR10_MEAN, std=CIFAR10_STD)
     ])
 
-    return trin_transform, test_transform
+    return train_transform, test_transform
 
 
 def get_dataloaders(batch_size:int, data_root:str):