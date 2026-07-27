b32c6d9b3eb331d7cac88d99763c42e69efe102c
城戸 洸之介 <kounosuke0704@icloud.com>
2026-07-28 03:07:29 +0900

    revice main.py

---
+++ trainer.py
@@ -0,0 +1,56 @@
+import torch
+from tqdm import tqdm
+
+def train(model, train_loader, criterion, optimizer, device):
+    model.train()
+
+    n_train = 0
+    n_correct = 0
+    total_loss = 0
+
+    for images, labels in tqdm(train_loader):
+        images = images.to(device)
+        laels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item() * (len(labels))
+        predicitions = outputs.argmax(dim=1)
+        n_correct += (predicitions==labels).sum().item()
+        n_train += len(labels)
+
+    train_acc = n_correct / n_train
+    train_loss = total_loss / n_train
+    return train_loss, train_acc
+
+
+@torch.no_grad()
+def evaluate(model, test_loader, criterion, device):
+    model.eval()
+
+    n_val = 0
+    n_correct = 0
+    total_loss = 0
+
+    for images, labels in tqdm(test_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        total_loss += loss.item() * len(labels)
+        predicted = outputs.argmax(dim=1)
+        n_correct += (predicted==labels).sum().item()
+        n_val += len(labels)
+
+    test_acc = n_correct / n_val
+    test_loss = total_loss / n_val
+
+    return test_loss, test_acc
+
+        
+        
