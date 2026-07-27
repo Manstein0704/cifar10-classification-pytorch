8b7848f6bf2b16fa303e9552c909e8a296a409bf
城戸 洸之介 <kounosuke0704@icloud.com>
2026-07-28 00:33:36 +0900

    Update CIFAR-10 data pipeline

--- dataset.py
+++ dataset.py
@@ -4,23 +4,58 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 
 
-transforms_train = transforms.Compose([
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.ToTensor(),
-    transforms.Normalize(0.5, 0.5),
-    transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), inplace=False)
-])
 
-transforms_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(0.5, 0.5)
-])
+def get_transforms():
+    trin_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(0.5, 0.5, 0.5),
+        transforms.RandomErasing(p=0.3, scale=(0.02, 0.33), ratio=(0.3, 3.3), inplace=False)
+    ])
 
-data_root = "./data"
-train_dataset = datasets.CIFAR10(root=data_root, train=True, transform=transforms_train, download=True)
-test_dataset = datasets.CIFAR10(root=data_root, train=False, transform=transforms_test, download=True)
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(0.5, 0.5, 0.5)
+    ])
+
+    return trin_transform, test_transform
 
 
-batch_size = 32
-train_loader = DataLoader(dataset=train_dataset, batch_size = batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+def get_dataloaders(batch_size:int, data_root:str):
+    train_transform, test_transform = get_transforms()
+
+    train_dataset = datasets.CIFAR10(
+        root=data_root,
+        train=True,
+        transform=train_transform,
+        download=True)
+    
+    test_dataset = datasets.CIFAR10(
+        root=data_root,
+        train=False,
+        transform=test_transform,
+        download=True)
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+    return train_loader, test_loader
+
+
+
+    
+
+
+
+
+
